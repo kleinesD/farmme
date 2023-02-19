ada40892f6c0b08efe51dfdf3457e9804f770307
Denny Kleines <kleinesdenny@gmail.com>
2023-02-19 21:50:46 +0300

    Daily commit. Desktop. 19.02.2023

--- public/js/milkingProjection.js
+++ public/js/milkingProjection.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getMilkingProjection = async (animalId) => {
-    let animal = [], farmAnimalsRes = [], allAnimalsRes =  [];
+    let animalRes = [], farmAnimalsRes = [], allAnimalsRes = [];
 
     /* GET ALL REQUIRED DATA */
     let data;
@@ -19,28 +19,105 @@ export const getMilkingProjection = async (animalId) => {
         console.log(err);
     }
 
+    /* MOVING ACTUALL RESULTS TO ARRAYS */
+    data.animal.milkingResults.forEach(res => {
+        if (res.lactationNumber) {
+            animalRes.push({
+                result: parseFloat(res.result),
+                date: new Date(res.date),
+                lactation: parseFloat(res.lactationNumber),
+                lactationStart: new Date(data.animal.lactations.find(el => el.number === res.lactationNumber).startDate)
+            });
+        }
+    });
+
     data.allAnimals.forEach(animal => {
         animal.milkingResults.forEach(res => {
-            console.log(animal, res.lactationNumber)
-            allAnimalsRes.push({
-                result: parseFloat(res.result),
-                date: new Date(res.date),
-                lactation: parseFloat(res.lactationNumber),
-                lactationStart: new Date(animal.lactations[res.lactationNumber - 1].startDate)
-            });
-        });
-    });
-    data.farmAnimals.forEach(animal => {
-        animal.milkingResults.forEach(res => {
-            farmAnimalsRes.push({
-                result: parseFloat(res.result),
-                date: new Date(res.date),
-                lactation: parseFloat(res.lactationNumber),
-                lactationStart: new Date(animal.lactations[res.lactationNumber - 1].startDate)
-            });
+            if (res.lactationNumber) {
+                allAnimalsRes.push({
+                    result: parseFloat(res.result),
+                    date: new Date(res.date),
+                    lactation: parseFloat(res.lactationNumber),
+                    lactationStart: new Date(animal.lactations.find(el => el.number === res.lactationNumber).startDate)
+                });
+
+                console.log(new Date(animal.lactations.find(el => el.number === res.lactationNumber).startDate));
+            }
         });
     });
 
-    console.log(animal, farmAnimalsRes, allAnimalsRes);
+    data.farmAnimals.forEach(animal => {
+        animal.milkingResults.forEach(res => {
+            if (res.lactationNumber) {
+                farmAnimalsRes.push({
+                    result: parseFloat(res.result),
+                    date: new Date(res.date),
+                    lactation: parseFloat(res.lactationNumber),
+                    lactationStart: new Date(animal.lactations.find(el => el.number === res.lactationNumber))
+                });
+            }
+        });
+    });
+
+    console.log(farmAnimalsRes)
+
+    /* SORTING ALL ARRAYS BY LACTATION NUMBER AND MONTH INTO THE LACTATION */
+    let animalResSorted = [], farmAnimalsResSorted = [], allAnimalsResSorted = [];
+
+    animalRes.forEach((res) => {
+        let month = Math.round((res.date.getTime() - res.lactationStart.getTime()) / 30 / 24 / 60 / 60 / 1000);
+        if(animalResSorted.length === 0) {
+            animalResSorted.push({
+                lactation: res.lactation,
+                monthIn: month,
+                results: [res]
+            });
+        } else {
+            let push = true;
+            animalResSorted.forEach(resSort => {
+                if(resSort.lactation === res.lactation && resSort.monthIn === month) {
+                    resSort.result.push(res);
+                    push = false;
+                }
+            });
+
+            if(push) {
+                animalResSorted.push({
+                    lactation: res.lactation,
+                    monthIn: month,
+                    results: [res]
+                });
+            }
+        }
+    });
+
+    farmAnimalsRes.forEach((res) => {
+        let month = Math.round((res.date.getTime() - res.lactationStart.getTime()) / 30 / 24 / 60 / 60 / 1000);
+        if(farmAnimalsResSorted.length === 0) {
+            farmAnimalsResSorted.push({
+                lactation: res.lactation,
+                monthIn: month,
+                results: [res]
+            });
+        } else {
+            let push = true;
+            farmAnimalsResSorted.forEach(resSort => {
+                if(resSort.lactation === res.lactation && resSort.monthIn === month) {
+                    resSort.result.push(res);
+                    push = false;
+                }
+            });
+
+            if(push) {
+                farmAnimalsResSorted.push({
+                    lactation: res.lactation,
+                    monthIn: month,
+                    results: [res]
+                });
+            }
+        }
+    });
+
+    
 
 };
